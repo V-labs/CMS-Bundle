bfd1e56d7a6fff12eda95cfc365e0f5bb90398f7
Benoît Bourgeois <benoit@v-labs.fr>
2016-12-12 10:58:41 +0100

    add help modal

---
+++ Resources/public/js/summernote/vlabs-help.js
@@ -0,0 +1,40 @@
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(window.jQuery);
+    }
+}(function ($) {
+
+    $.extend($.summernote.plugins, {
+        'vlabs-help': function (context) {
+
+            context.memo('button.vlabs-help', function () {
+
+                return $.summernote.ui.button({
+                    contents: '<i class="fa fa-question-circle"></i>',
+                    tooltip: 'Aide',
+                    click: function () {
+
+                        $.ajax({
+                            url: Routing.generate('vlabs_cms_admin_summernote_modal', {
+                                slug: 'help'
+                            }),
+                            type: 'GET',
+                            success: function (data) {
+                                var $modal = $(data);
+                                $modal.find('.close').click(function(){
+                                    $modal.fadeOut();
+                                });
+                                $('body').append($modal);
+                                $modal.fadeIn();
+                            }
+                        });
+                    }
+                }).render();
+            });
+        }
+    });
+}));
